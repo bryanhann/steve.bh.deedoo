2cbb6136b7937e0364d8018f5e78170aba5687eb
Bryan Hann <nobody@nowhere>
2023-05-24 16:16:44 -0230

    Refactor.

--- bin/lib/deedoo/server.py
+++ bin/lib/deedoo/server.py
@@ -7,8 +7,26 @@ from pathlib import Path
 import deedoo.stoppers as SS
 import deedoo.util as UTIL
 
-ALARM_LIST = []
-DING_LIST = []
+ALARMS = []
+DINGS = []
+
+def poll_once(stop):
+    SS.killed()
+    assert stop
+    if stop in ALARMS:
+        return True
+    if SS.found(stop):
+        UTIL.say( f'stopped' )
+        ALARMS.append(stop)
+        return True
+    return False
+
+def poll(stop, seconds):
+    slices = 10
+    for _ in range(seconds*slices):
+        if poll_once(stop):
+            return True
+        time.sleep(1.0/slices)
 
 def run_server(DEBUG=False):
     SS.init()
@@ -26,19 +44,14 @@ class Now:
         self.dt = dt
         self.hh = dt.strftime("%H")
         self.mm = dt.strftime("%M")
-        self.hhmm = dt.strftime("%H%M")
-        self.hhmmss = dt.strftime("%H%M%S")
-        self.hour = dt.hour
-        self.minute = dt.minute
-        self.second = dt.second
-        self.show = self.hhmm
-        self.alarm_sleep = 5
+        self.show = dt.strftime("%H%M")
+        self._alarm_sleep = 5
         self._ding = self.dt.minute % 5 == 0
         self._alarm = self.dt.minute % 15 == 0
 
         if self.DEBUG:
-            self.show=self.hhmmss
-            self.alarm_sleep=1
+            self.show = dt.strftime("%H%M%S")
+            self._alarm_sleep=1
             self._ding = self.dt.second % 5 == 0
             self._alarm = self.dt.second % 15 == 0
 
@@ -53,33 +66,26 @@ class Now:
             return f"{self.hh} {self.mm}"
 
     def main(self):
-        self.debug_report()
-        self.ding()
-        self.alarm()
+        if self.DEBUG:
+            UTIL.errout(str(self)+'    ' +'\n')
+        if self._ding:
+            self.ding()
+        if self._alarm:
+            self.alarm()
 
     def ding(self):
-        if not self._ding:
-            return
-        if self.stop in DING_LIST:
-            return
-        DING_LIST.append(self.stop)
-        UTIL.say(f"The time is {self._phone()}")
+        if not self.stop in DINGS:
+            DINGS.append(self.stop)
+            UTIL.say(f"The time is {self._phone()}")
 
     def alarm(self):
-        if not self._alarm:
-            return
-        if self.stop in ALARM_LIST:
-            return
-        ALARM_LIST.append(self.stop)
+        seconds = self._alarm_sleep
         for count in range(self.MISSES, 0, -1):
-            if SS.killed():
+            if poll(self.stop, seconds):
                 return
-            if SS.found(self.stop):
-                UTIL.say( f'stopped' )
-                return
-            UTIL.say( str(count) )
-            UTIL.sayspell(self.stop)
-            time.sleep(self.alarm_sleep)
+            UTIL.say( f'deedoo {count}' )
+        UTIL.say('giving up')
+        exit()
 
     def __repr__(s):
         b = f"{s.stop=}"
@@ -88,6 +94,6 @@ class Now:
         t = s.dt.strftime('%H:%M:%S')
         return f"{t} {b} {c} {d}"
 
-    def debug_report(self):
-        if self.DEBUG:
-            UTIL.errout(str(self)+'    ')
+
+
+
